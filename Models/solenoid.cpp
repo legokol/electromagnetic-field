e78f086b50b0dfc5e369d19d67c74084308acd26
Legokol <legokol3@gmail.com>
2021-04-08 00:26:18 +0300

    Name fix.

--- Models/solenoid.cpp
+++ Models/solenoid.cpp
@@ -4,18 +4,18 @@
 int main() {
     int n = 100;
     double h = 5;
-    vector<ConductorElement> ring;
+    vector<ConductorElement> solenoid;
     for (int j = 0; j < 11; ++j) {
         for (int i = 0; i < 8; ++i) {
             vector3D loc((n - 1) * h / 2 + cos(i * M_PI / 4) * h / 2, (n - 1) * h / 2 + sin(i * M_PI / 4) * h / 2,
                          (n - 1) * h / 2 + (j - 5) * h);
             vector3D I(-sin(i * M_PI / 4), cos(i * M_PI / 4), 0);
-            ring.push_back(ConductorElement(loc, I, 12.5));
+            solenoid.push_back(ConductorElement(loc, I, 12.5));
         }
     }
     CalcMesh mesh(n, h);
     cout << "Initialization finished, starting calculation" << endl;
-    mesh.calculate(ring);
+    mesh.calculate(solenoid);
     mesh.calculateGrad();
     cout << "Calculation finished, starting saving" << endl;
     mesh.snapshot("solenoid.vts");
